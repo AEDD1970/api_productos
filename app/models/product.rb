70c6703b96a81393094be25c24a4f4b57bb68d26
AEDD1970 <ing.alexis.duque@gmail.com>
2019-07-04 17:36:58 -0500

    Validation of name and current price, unique and type number is uncovered

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
     validates :producname, presence:true
-    validates: price, presence:true
     validates :producname, uniquenes: { 
     message: "There is already a product with that name" }
     validates :price, format: { with: /\A\d+\z/, message: "Integer only. No sign allowed." }
