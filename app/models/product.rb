6387bd19bdf009706406410c553df4259242e2e3
AEDD1970 <ing.alexis.duque@gmail.com>
2019-07-05 09:29:28 -0500

    validation produc name presence

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
     validates :producname, presence:true
-    validates :producname, uniquenes: true
+    validates :producname, presence: true
     validates :price, format: { with: /\A[+-]?\d+\z/, message: "Integer only. No sign allowed." }
 end
