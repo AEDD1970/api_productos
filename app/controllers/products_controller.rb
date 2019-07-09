6e48367cdc6d6544aa5a9a93eab105b10d3eb496
AEDD1970 <ing.alexis.duque@gmail.com>
2019-07-09 08:40:56 -0500

    name estructure

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,14 +5,21 @@ class ProductsController < ApplicationController
   # GET /products
   def index
     @products = Product.all.select(:id, :producname, :price, :description)
+data = [
+  data: @products.as_json
 
-    render json: @products
+]
+render json: data
+
   end
 
   # GET /products/1
   def show
     render json: @product
+
+    #GET /prdoducts/nameproduct
   end
+     # pass unique product
   def show_uniquess
     render json: @product
   end
@@ -49,11 +56,12 @@ class ProductsController < ApplicationController
     def set_product
       @product = Product.find(params[:id])
     end
-   
+   # pass unique product
     def product_uniquess
       @product = Product.find_by(producname: params[:producname])
 
     end
+  
   
     # Only allow a trusted parameter "white list" through.
     def product_params
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,4 @@
 class Product < ApplicationRecord
     validates :producname, presence:true
-    validates :producname, presence: true
     validates :price, format: { with: /\A[+-]?\d+\z/, message: "Integer only. No sign allowed." }
 end
