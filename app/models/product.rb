ac43b4684dafd8d9eca4a68339b4ecf1b0fa7ed2
AEDD1970 <ing.alexis.duque@gmail.com>
2019-07-19 10:49:09 -0500

    validate uniqueness 19.07.19

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
     validates :producname, presence:true
+    validates :producname, uniqueness:true
     validates :price, format: { with: /\A[+-]?\d+\z/, message: "Integer only. No sign allowed." }
 end
